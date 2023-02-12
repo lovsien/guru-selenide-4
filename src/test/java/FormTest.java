d68be69a5cb61f0aec525d0f5df7000ca77b26ce
lovsien <loramlm42@gmail.com>
2023-02-12 22:51:52 +0700

    Added assertions

--- src/test/java/FormTest.java
+++ src/test/java/FormTest.java
@@ -1,44 +1,82 @@
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class FormTest {
 
+    String formURL = "/automation-practice-form";
+    String firstName = "Tanya";
+    String lastName = "Garbaruk";
+    String email = "test@email.com";
+    String userNumber = "1234567890";
+    String gender = "Female";
+    String yearValue = "2000";
+    String monthValue = "May";
+    String dayValue = "04";
+    String subject = "English";
+    String hobby = "Reading";
+    File photo = new File("src/test/resources/profile_photo.jpg");
+    String photoName = "profile_photo.jpg";
+    String currentAddress = "2134 Willison Street";
+    String state = "NCR";
+    String city = "Delhi";
+
+    @BeforeAll
+    static void setUp() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+    }
+
     @Test
     void submitCompletedFormSuccessfullyTest() {
-        open("https://demoqa.com/automation-practice-form");
+        open(formURL);
         $("div.practice-form-wrapper h5").shouldHave(text("Student Registration Form"));
 
-        $("#firstName").setValue("Tanya");
-        $("#lastName").setValue("Garbaruk");
-
-        $("#userEmail").setValue("test@email.com");
-        $("[for=gender-radio-2]").click();
-        $("#userNumber").setValue("1234567890");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
+        $(byText(gender)).click();
+        $("#userNumber").setValue(userNumber);
 
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__year-select").selectOptionByValue("2000");
-        $(".react-datepicker__month-select").selectOptionContainingText("May");
-        $(".react-datepicker__day--004").click();
+        $(".react-datepicker__year-select").selectOptionByValue(yearValue);
+        $(".react-datepicker__month-select").selectOptionContainingText(monthValue);
+        $(".react-datepicker__day--0" + dayValue + ":not(.react-datepicker__day--outside-month)").click();
 
-        $("#subjectsInput").setValue("English").pressEnter();
-        $("[for=hobbies-checkbox-2]").click();
-
-        File photo = new File("src/test/resources/profile_photo.jpg");
+        $("#subjectsInput").setValue(subject).pressEnter();
+        $(byText(hobby)).click();
         $("#uploadPicture").uploadFile(photo);
 
-        $("#currentAddress").setValue("Lorem ipsum dolor");
-
+        $("#currentAddress").setValue(currentAddress);
         $("#state").scrollTo();
-        $$(".css-1wy0on6").get(1).click();
-        $("#react-select-3-option-0").click();
+        $(byText("Select State")).click();
+        $(byText(state)).click();
 
-        $$(".css-1wy0on6").get(2).click();
-        $("#react-select-4-option-0").click();
+        $(byText("Select City")).click();
+        $(byText(city)).click();
+
+//        executeJavaScript("$('#fixedban').remove()");
+//        executeJavaScript("$('footer').remove()");
 
         $("#submit").shouldBe(visible).click();
+
+        $(".modal-dialog").should(appear);
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $x("//td[text()='Student Name']/following-sibling::*").shouldHave(text(firstName + " " + lastName));
+        $x("//td[text()='Student Email']/following-sibling::*").shouldHave(text(email));
+        $x("//td[text()='Gender']/following-sibling::*").shouldHave(text(gender));
+        $x("//td[text()='Mobile']/following-sibling::*").shouldHave(text(userNumber));
+        $x("//td[text()='Date of Birth']/following-sibling::*").shouldHave(text(dayValue + " " + monthValue + "," + yearValue));
+        $x("//td[text()='Subjects']/following-sibling::*").shouldHave(text(subject));
+        $x("//td[text()='Hobbies']/following-sibling::*").shouldHave(text(hobby));
+        $x("//td[text()='Picture']/following-sibling::*").shouldHave(text(photoName));
+        $x("//td[text()='Address']/following-sibling::*").shouldHave(text(currentAddress));
+        $x("//td[text()='State and City']/following-sibling::*").shouldHave(text(state + " " + city));
     }
 }
